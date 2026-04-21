f2dce724c88fa2c7d3e4347333e4d45b8d5e0111
Emanuel Mozo <e.mozo89@gmai.com>
2026-04-21 14:25:19 -0400

    actualizacion

--- ejercicio1_ciclos.py
+++ ejercicio1_ciclos.py
@@ -1,16 +1,35 @@
+#print("---Bienvenido al programa de registro curricular---")
+#num1 = float(input("Ingrese su nota:"))
+#num2 = float(input("Ingrese su nota:"))
+#num3 = float(input("Ingrese su nota:"))
+#num4 = float(input("Ingrese su nota:"))
+#num5 = float(input("Ingrese su nota:"))
+#num6 = float(input("Ingrese su nota:"))
+#num7 = float(input("Ingrese su nota:"))
+
+#prom = (num1+num2+num3+num4+num5+num6+num7) / 7
+
+#if prom >=4:
+#    print(f"Su promedio {prom:.1f} es aceptable")
+#else:
+#    print(f"Su promedio {prom:.1f} no es suficiente para aprobar")
+#print("fin")    
+
 print("---Bienvenido al programa de registro curricular---")
-num1 = float(input("Ingrese su nota:"))
-num2 = float(input("Ingrese su nota:"))
-num3 = float(input("Ingrese su nota:"))
-num4 = float(input("Ingrese su nota:"))
-num5 = float(input("Ingrese su nota:"))
-num6 = float(input("Ingrese su nota:"))
-num7 = float(input("Ingrese su nota:"))
 
-prom = (num1+num2+num3+num4+num5+num6+num7) / 7
+suma = 0
+i = 1
 
-if prom >=4:
-    print(f"Su promedio {prom} es aceptable")
+while i <= 7:
+    nota = float(input(f"Ingrese la nota {i}: "))
+    suma = suma + nota
+    i += 1
+
+prom = suma / 7
+
+if prom >= 4:
+    print(f"Su promedio {prom:.1f} es aprobado")
 else:
-    print(f"Su promedio {prom} no es suficiente para aprobar")
-print("fin")    
+    print(f"Su promedio {prom:.1f} es reprobado")
+
+print("fin")
